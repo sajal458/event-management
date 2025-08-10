fd9fde3d6b2b95ad8a07697c498ab4e2624dcccf
sajal458 <shsajal8561@gmail.com>
2025-08-10 21:06:13 +0600

    final1

--- Event/forms.py
+++ Event/forms.py
@@ -18,15 +18,15 @@ class TaskModel(forms.Form):
         })
     )
     date = forms.DateField(
-    label="Event date",
-    widget=forms.SelectDateWidget(attrs={
-        "class": "inline-block w-auto mx-1 px-2 py-2 border border-gray-300 rounded-md"
-    })
+        label="Event date",
+        widget=forms.SelectDateWidget(attrs={
+            "class": "inline-block w-auto mx-1 px-2 py-2 border border-gray-300 rounded-md"
+        })
     )
     time = forms.TimeField(
         label="Event time",
         widget=forms.TimeInput(format='%H:%M', attrs={
-            "placeholder":"HH-MM",
+            "placeholder": "HH-MM",
             "class": "w-full px-4 py-2 border border-gray-300 rounded-md "
         })
     )
@@ -40,7 +40,6 @@ class TaskModel(forms.Form):
     category = forms.ChoiceField(
         label="Category",
         widget=forms.Select(attrs={
-            
             "class": "w-full px-4 py-2 border border-gray-300 rounded-md "
         })
     )
@@ -50,6 +49,14 @@ class TaskModel(forms.Form):
         }),
         label="Participants"
     )
+
+    def __init__(self, *args, **kwargs):
+        participant = kwargs.pop("participant", [])
+        category = kwargs.pop("category", [])
+        super().__init__(*args, **kwargs)
+
+        self.fields["participant"].choices = [(p.id, p.name) for p in participant]
+        self.fields["category"].choices = [(c.id, c.name) for c in category]
 
 
 class ParticipantForm(forms.ModelForm):
@@ -62,6 +69,7 @@ class ParticipantForm(forms.ModelForm):
             'events': forms.CheckboxSelectMultiple(attrs={"class": "space-y-2"})
         }
 
+
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
@@ -70,12 +78,3 @@ class CategoryForm(forms.ModelForm):
             'name': forms.TextInput(attrs={"class": "w-full px-4 py-2 border border-gray-300 rounded-md"}),
             'description': forms.Textarea(attrs={"class": "w-full px-4 py-2 border border-gray-300 rounded-md"})
         }
-
-
-    def __init__(self, *args, **kwargs):
-        participant = kwargs.pop("participant", [])
-        category = kwargs.pop("category", [])
-        super().__init__(*args, **kwargs)
-        
-        self.fields["participant"].choices = [(p.id, p.name) for p in participant]
-        self.fields["category"].choices = [(c.id, c.name) for c in category]
