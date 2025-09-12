6e4dd70c920bab6ca071bd822b7f4d300aca0492
sajal458 <shsajal8561@gmail.com>
2025-09-12 18:37:31 +0600

    last3

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -30,7 +30,7 @@ MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGIN =['https://*.onrender.com','http://127.0.0.1:8000']
+# CSRF_TRUSTED_ORIGIN =['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # ALLOWED_HOSTS = [
@@ -45,10 +45,11 @@ CSRF_TRUSTED_ORIGIN =['https://*.onrender.com','http://127.0.0.1:8000']
 # ]
 
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "https://event-management-xlch.onrender.com",
-#     "http://127.0.0.1:8000"
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    # "https://event-management-xlch.onrender.com",
+    "https://event-management-1-afm4.onrender.com",
+    "http://127.0.0.1:8000"
+]
 
 # Application definition
 
