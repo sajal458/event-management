f94430cbe4db7f12a1a6be2fdff9c5c4f6285c07
sajal458 <shsajal8561@gmail.com>
2025-09-12 18:21:24 +0600

    last1

--- Event_Management/settings.py
+++ Event_Management/settings.py
@@ -29,15 +29,15 @@ DEBUG = True
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR / 'media'
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 # CSRF_TRUSTED_ORIGIN =['https://*.onrender.com','http://127.0.0.1:8000']
 
 
-# ALLOWED_HOSTS = [
-#     'event-management-xlch.onrender.com',
-#     '127.0.0.1',
-#     'localhost'
-# ]
+ALLOWED_HOSTS = [
+    'event-management-xlch.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # ALLOWED_HOSTS = [
