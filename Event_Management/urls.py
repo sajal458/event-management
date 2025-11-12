4ec6460a970185bdae0524a931e55e4fab2ae701
sajal458 <shsajal8561@gmail.com>
2025-11-12 17:51:23 +0600

    final2

--- Event_Management/urls.py
+++ Event_Management/urls.py
@@ -21,9 +21,9 @@ from core.views import homes,no_permission
 from django.conf.urls.static import static
 from django.conf import settings
 from Users.views import create_admin
-from . import views
+
 urlpatterns = [
-    path("create-admin/", views.create_admin),
+    path("create-admin/",create_admin),
     path('admin/', admin.site.urls),
     #path('', lambda request: redirect('home')), 
     path('event/', include("Event.urls")),
